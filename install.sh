0a762ab32b3e44effad31e1adb1762d0c3b2b820
Amit <amitkulkarni2028@gmail.com>
2022-08-24 01:06:54 +0530

    improve script

--- install.sh
+++ install.sh
@@ -13,10 +13,12 @@ export FLINT_HOME_DIR=$HOME/.local/bin/
 
 
 FLINT_GITHUB_URL="https://api.github.com/repos/moja-global/install/releases/latest"
-FLINT_LATEST_VERSION=$(curl $FLINT_GITHUB_URL | jq -r '.assets[].browser_download_url')
 
 OPT_INSTALL=0
+OPT_INSTALL_DEV=0
 OPT_UNINSTALL=0
+OPT_UPDATE=0
+OPT_HELP=0
 
 function usage() {
 	printf "Install script for FLINT AppImages\n"
@@ -39,15 +41,9 @@ if [ $# -ne 0 ]; then
 			if [[ "$1" = 'dev' ]]; then
 				OPT_INSTALL_DEV=1
 			fi
-			if [[ "$1" = 'stable' ]]; then
-				OPT_INSTALL_STABLE=1
-			fi
-		else
-			print_usage
 		fi
 
 		if [[ "$1" = '--uninstall' ]]; then
-			echo "UNINSTALL"
 			OPT_UNINSTALL=1
 		fi
 		if [[ "$1" = '--update' || "$1" = '-u' ]]; then
@@ -55,56 +51,98 @@ if [ $# -ne 0 ]; then
 			OPT_UPDATE=1
 		fi
 		if [[ "$1" = '--help' || "$1" = '-h' ]]; then
-			echo "HELP"
 			OPT_HELP=1
 		fi
 
 		shift
 	done
 else 
-	print_usage
+	usage
 fi
 
 
-# Fetch a release from one of the Github releases
+# Fetch the latest FLINT AppImage release
 fetch_release() {
-	HOME_DIR=$USER/Downloads
-	
-	if $OPT_INSTALL_STABLE == 1; then
-		GITHUB_RELEASE_URL="https://github.com"
-	else
-		GITHUB_RELEASE_URL
-	fi
-
-
+	HOME_DIR=$HOME/Downloads
 	mkdir -p $HOME_DIR
 
+	if [ $OPT_INSTALL_DEV -eq 1 ]; then
+		release='dev'
+	fi
+
+	echo "Fetching latest version of FLINT"
+
+	#UBUNTU_VERSION=$(cat /etc/os-release | grep ^VERSION_ID | cut -d '=' -f2 | tr -d '"')
+	UBUNTU_VERSION="22.04"
+
+	# Get latest release URL
+	FLINT_LATEST_VERSION=$(curl -sS $FLINT_GITHUB_URL | jq -r '.assets[].browser_download_url' | grep $UBUNTU_VERSION)
+	
+	# TODO: What will be the value of `release` in case of stable releases?
+	GITHUB_RELEASE_URL="https://github.com/moja-global/install/releases/download/$release/FLINT-ubuntu-$UBUNTU_VERSION.AppImage"
+
+	curl -L $GITHUB_RELEASE_URL --output $HOME_DIR/FLINT.AppImage
+
+	# Check if AppImage is empty or does not exist. 
+	if [ -s $HOME_DIR/FLINT.AppImage ]; then
+		echo -e "\n\nAppImage fetched successfully!"
+	else
+		echo "Failed to fetch AppImage"
+	fi
+}
+
+check_latest_version() {
+	version=$(curl -L -Ss $FLINT_GITHUB_URL | jq ".tag_name" | cut -d 'v' -f2 | tr -d '"')
+	echo $version
 }
 
 main() {
-	if $OPT_INSTALL == 1; then
-		fetch_release
+	which jq
+	if [ $? -eq 0 ]; then
+		echo "jq not found. Please install jq first"
+		exit
 	fi
 
-	if $OPT_UNINSTALL == 1; then
-		if test -f $USER/.local/bin/flint.AppImage == 0; then
-			rm $USER/.local/bin/flint.AppImage
+	if [ $OPT_INSTALL -eq 1 ]; then
+		test -f $APPIMAGE_DIR/FLINT.AppImage
+		if [ $? -eq 0 ]; then
+			echo "FLINT installation is already present on the system"
+			exit
+		fi
+
+		fetch_release
+
+		APPIMAGE_DIR=/home/$USER/.local/bin
+		mkdir -p $APPIMAGE_DIR
+		mv /home/$USER/Downloads/FLINT.AppImage $APPIMAGE_DIR
+		chmod +x $APPIMAGE_DIR/FLINT.AppImage
+		$APPIMAGE_DIR/FLINT.AppImage 1>/dev/null
+
+		if [ $? -eq 0 ]; then
+			echo -e "\n\nInstalled FLINT AppImage at $APPIMAGE_DIR. Please make sure the folder is included in your PATH variable."
 		else
-			echo "FLINT Installation not found!"
+			echo -e "\n\nFLINT installation failed!"
+			exit
 		fi
 	fi
 
-	if $OPT_UPDATE == 1; then
-		check_version
+	if [ $OPT_UNINSTALL -eq 1 ]; then
+		test -f $FLINT_HOME_DIR/FLINT.AppImage
+		if [ $? == 0 ]; then
+			rm $FLINT_HOME_DIR/FLINT.AppImage
+			echo "Uninstalled FLINT AppImage successfully!"
+		else
+			echo "FLINT installation not found!"
+		fi
 	fi
 
-	if $OPT_HELP == 1; then
+	if [ $OPT_UPDATE -eq 1 ]; then
+		latest_version=$(check_version)
+	fi
+
+	if [ $OPT_HELP -eq 1 ]; then
 		usage
 	fi
-
-	APPIMAGE_DIR=$USER/.local/bin
-	mkdir -p $APPIMAGE_DIR
-	mv 
 }
 
 main
